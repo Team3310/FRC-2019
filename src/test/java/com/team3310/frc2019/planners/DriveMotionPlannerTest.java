8e73c7ba3d045c880ce707c09e888e3745d5138b
BrianSelle <bselle@cebridge.net>
2019-03-31 20:46:32 -0500

    Added some tests to figure out path issue

--- src/test/java/com/team3310/frc2019/planners/DriveMotionPlannerTest.java
+++ src/test/java/com/team3310/frc2019/planners/DriveMotionPlannerTest.java
@@ -18,6 +18,10 @@ import frc.team3310.utility.lib.trajectory.timing.CentripetalAccelerationConstra
 
 public class DriveMotionPlannerTest {
 
+    private Pose2d kCargoSideTurnToLoadingPose = new Pose2d(new Translation2d(295.75, -60.5), Rotation2d.fromDegrees(-180.00));
+    private Pose2d kLoadingPose = new Pose2d(new Translation2d(30.00, -130), Rotation2d.fromDegrees(-180.00));
+
+
     @Test
     public void testStraight() {
         TrajectoryGenerator.getInstance().generateTrajectories();
@@ -85,6 +89,27 @@ public class DriveMotionPlannerTest {
     }
 
     @Test
+    public void testForwardWithTheta180() {
+
+        DriveMotionPlanner motion_planner = new DriveMotionPlanner();
+        motion_planner.setFollowerType(DriveMotionPlanner.FollowerType.NONLINEAR_FEEDBACK);
+        motion_planner.setTrajectory(new TrajectoryIterator<>(new TimedView<>(motion_planner.generateTrajectory
+                (false, Arrays.asList(kCargoSideTurnToLoadingPose, kLoadingPose),
+                        null,
+                        120.0, 120.0, 10.0))));
+        double t = 0.0;
+        Pose2d pose = motion_planner.setpoint().state().getPose();
+        System.out.println(motion_planner.toCSVHeader());
+        while (!motion_planner.isDone()) {
+            motion_planner.update(t, pose);
+            pose = motion_planner.mSetpoint.state().getPose();//.transformBy(new Pose2d(new Translation2d(0.0, -1.0),
+            // Rotation2d.fromDegrees(-2.0)));
+            System.out.println(motion_planner.toCSV());
+            t += 0.01;
+        }
+    }
+
+    @Test
     public void testReverseSwerveLeft() {
         DriveMotionPlanner motion_planner = new DriveMotionPlanner();
         motion_planner.setTrajectory(new TrajectoryIterator<>(new TimedView<>(motion_planner.generateTrajectory
@@ -144,6 +169,70 @@ public class DriveMotionPlannerTest {
             System.out.println(fwd_motion_planner.toCSV() + "," + rev_motion_planner.toCSV());
             t += dt;
         }
+    }
+
+    @Test
+    public void testForwardReverseSame180Theta() {
+        DriveMotionPlanner fwd_motion_planner = new DriveMotionPlanner();
+        fwd_motion_planner.setFollowerType(DriveMotionPlanner.FollowerType.NONLINEAR_FEEDBACK);
+        fwd_motion_planner.setTrajectory(new TrajectoryIterator<>(new TimedView<>(fwd_motion_planner.generateTrajectory
+                (false, Arrays.asList(kCargoSideTurnToLoadingPose, kLoadingPose),
+                        null,
+                        120.0, 120.0, 5.0))));
+        DriveMotionPlanner rev_motion_planner = new DriveMotionPlanner();
+        rev_motion_planner.setFollowerType(DriveMotionPlanner.FollowerType.NONLINEAR_FEEDBACK);
+        rev_motion_planner.setTrajectory(new TrajectoryIterator<>(new TimedView<>(rev_motion_planner.generateTrajectory
+                (true, Arrays.asList(kLoadingPose, kCargoSideTurnToLoadingPose),
+                        null,
+                        120.0, 120.0, 5.0))));
+
+        final double dt = 0.01;
+        double t = 0.0;
+        Pose2d start_error_fwd = new Pose2d(2.0, 3.0, Rotation2d.fromDegrees(2.0));
+        Pose2d start_error_rev = new Pose2d(-2.0, 3.0, Rotation2d.fromDegrees(-2.0));
+        Pose2d fwd_pose = fwd_motion_planner.setpoint().state().getPose().transformBy(start_error_fwd);
+        Pose2d rev_pose = rev_motion_planner.setpoint().state().getPose().transformBy(start_error_rev);
+        while (!fwd_motion_planner.isDone() || !rev_motion_planner.isDone()) {
+            DriveMotionPlanner.Output fwd_output = fwd_motion_planner.update(t, fwd_pose);
+            Twist2d fwd_delta = Kinematics.forwardKinematics(fwd_output.left_velocity * dt * Constants
+                    .kDriveWheelDiameterInches / 2.0, fwd_output.right_velocity * dt * Constants
+                    .kDriveWheelDiameterInches / 2.0);
+            fwd_pose = fwd_pose.transformBy(Pose2d.exp(fwd_delta));
+            //System.out.println("FWD Delta: " + fwd_delta + ", Pose: " + fwd_pose);
+            DriveMotionPlanner.Output rev_output = rev_motion_planner.update(t, rev_pose);
+            Twist2d rev_delta = Kinematics.forwardKinematics(rev_output.left_velocity * dt * Constants
+                    .kDriveWheelDiameterInches / 2.0, rev_output.right_velocity * dt * Constants
+                    .kDriveWheelDiameterInches / 2.0);
+            rev_pose = rev_pose.transformBy(Pose2d.exp(rev_delta));
+            //System.out.println("REV Delta: " + rev_delta + ", Pose: " + rev_pose);
+            System.out.println(fwd_motion_planner.toCSV() + "," + rev_motion_planner.toCSV());
+            t += dt;
+        }
+    }
+
+    @Test
+    public void testFollowerReachesGoalTheta180() {
+        final DriveMotionPlanner motion_planner = new DriveMotionPlanner();
+        motion_planner.setFollowerType(DriveMotionPlanner.FollowerType.PURE_PURSUIT);
+        motion_planner.setTrajectory(new TrajectoryIterator<>(new TimedView<>(motion_planner.generateTrajectory(false,
+                Arrays.asList(kCargoSideTurnToLoadingPose, kLoadingPose), null, 120.0, 120.0, 10.0))));
+        final double dt = 0.01;
+        double t = 0.0;
+        Pose2d initial_error = new Pose2d(2.0, 3.0, Rotation2d.fromDegrees(3.5));
+        Pose2d pose = motion_planner.setpoint().state().getPose().transformBy(initial_error);
+        System.out.println(motion_planner.setpoint().toCSVHeader() + ", leftOutputVel, rightOutputVel," + initial_error.toCSVHeader());
+        while (!motion_planner.isDone()) {
+            DriveMotionPlanner.Output output = motion_planner.update(t, pose);
+            Twist2d delta = Kinematics.forwardKinematics(output.left_velocity * dt * Constants
+                    .kDriveWheelDiameterInches / 2.0, output.right_velocity * dt * Constants
+                    .kDriveWheelDiameterInches / 2.0);
+            // Add some systemic error.
+            delta = new Twist2d(delta.dx * 1.0, delta.dy * 1.0, delta.dtheta * 1.05);
+            pose = pose.transformBy(Pose2d.exp(delta));
+            t += dt;
+            System.out.println(motion_planner.setpoint().toCSV() + "," + output.left_velocity + "," + output.right_velocity + "," + motion_planner.error().toCSV());
+        }
+        System.out.println(pose);
     }
 
     @Test
--- src/test/java/com/team3310/lib/trajectory/PurePursuitControllerTest.java
+++ src/test/java/com/team3310/lib/trajectory/PurePursuitControllerTest.java
@@ -16,6 +16,8 @@ import frc.team3310.utility.lib.trajectory.PurePursuitController;
 import frc.team3310.utility.lib.trajectory.Trajectory;
 
 public class PurePursuitControllerTest {
+    private Pose2d kCargoSideTurnToLoadingPose = new Pose2d(new Translation2d(295.75, -60.5), Rotation2d.fromDegrees(-180.00));
+    private Pose2d kLoadingPose = new Pose2d(new Translation2d(30.00, -130), Rotation2d.fromDegrees(-180.00));
 
     @Test
     public void test() {
@@ -30,6 +32,30 @@ public class PurePursuitControllerTest {
                 arc_length_parameterized_trajectory, 1.0, 6.0, 0.1);
 
         Pose2d robot_pose = new Pose2d(waypoints.get(0), Rotation2d.identity());
+        final int kMaxIter = 100;
+        int i = 0;
+        for (; i < kMaxIter; ++i) {
+            if (controller.isDone())
+                break;
+            Twist2d steering_command = controller.steer(robot_pose);
+            steering_command = steering_command.scaled(1.0 / Math.max(1.0, steering_command.norm()));
+            System.out.println("Iter: " + i + ", Pose: " + robot_pose + ", Steering Command: " + steering_command);
+            robot_pose = robot_pose.transformBy(Pose2d.exp(steering_command));
+        }
+        assertTrue(i < kMaxIter);
+    }
+
+    @Test
+    public void testTheta180() {
+        List<Pose2d> waypoints = Arrays.asList(kLoadingPose, kCargoSideTurnToLoadingPose );
+
+        // Create the reference trajectory (straight line motion between waypoints).
+        Trajectory<Pose2d> reference_trajectory = new Trajectory<>(waypoints);
+        DistanceView<Pose2d> arc_length_parameterized_trajectory = new DistanceView<>(reference_trajectory);
+        PurePursuitController<Pose2d> controller = new PurePursuitController<>(
+                arc_length_parameterized_trajectory, 1.0, 6.0, 0.1);
+
+        Pose2d robot_pose = waypoints.get(0);
         final int kMaxIter = 100;
         int i = 0;
         for (; i < kMaxIter; ++i) {
